c205db164a05ccedeff9eb749699f96c7ecedf03
neuron909 <kaushikrb909@gmail.com>
2020-06-09 17:31:36 +0530

    removed handler

--- api/user/user.router.js
+++ api/user/user.router.js
@@ -1,10 +1,7 @@
 const router = require( 'express' ).Router() ;
-const handler = require( '../../handler.js' );
 const user = require( './user.controller' ) ;
-router.post( '/', ( req, res ) => { 
-    handler.send( res, 400, handler.errData.invalidReq ) ;
-} )
-router.post( '/login', user.login ) ;
+
+router.post( '/login', user.login   ) ;
 router.post( '/signup', user.signup ) ;
 
 module.exports.router = router
