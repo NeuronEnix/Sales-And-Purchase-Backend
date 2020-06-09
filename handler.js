8bcd51eb82c02970205ed25f175d1f80bac6428d
neuron909 <kaushikrb909@gmail.com>
2020-06-09 20:28:58 +0530

    code resturcture

--- handler.js
+++ handler.js
@@ -1,40 +1,17 @@
-function send( resObj, resCode, data ){
+module.exports.send = ( resObj, resCode, data ) => {
     let res;
-    if( resCode === 200 ){
-        res = {
-            code : 0,
-            status : 'OK',
-            data : data
-        } ;
-    }
-    else{
-        res = {
-            code : data.code,
-            status : 'FAILED',
-            msg : data.msg
-        } ;
-    }
+    if( resCode === 200 ) res = { code : 0,         status : 'OK',     data : data      } ;
+    else                  res = { code : data.code, status : 'FAILED', msg  : data.msg  } ;
     resObj.status( resCode ).send( res ) ;
 }
 
-errData = {
-    invalidReq : {
-        code : 1,
-        msg : 'invalid request'
-    },
-    invalidCredential :{
-        code : 2,
-        msg : 'incorrect user name or pass'
-    },
-    missingField : {
-        code : 3,
-        msg : 'Required Field Missing'
-    },
-    dupEmail : {
-        code : 4,
-        msg : 'Email already Exists'
-    }
+module.exports.errData = {
+    invalidReq          : { code : 1, msg : 'Invalid request'               },
+    invalidCredential   : { code : 2, msg : 'Incorrect user name or pass'   },
+    missingField        : { code : 3, msg : 'Required Field Missing'        },
+    dupEmail            : { code : 4, msg : 'Email already Exists'          }
 } ;
 
-module.exports.send = send
-module.exports.errData = errData
+module.exports.defRes = ( resObj, apiName ) => {
+    resObj.status( 400 ).send( { code : -1, status : 'FAILED', msg : 'In Progress', api : apiName } ) ;
+}
