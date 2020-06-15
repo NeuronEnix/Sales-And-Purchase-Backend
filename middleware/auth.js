ba74e01c0196855aae2fd1424c8bc9c40e971a45
neuron909 <kaushikrb909@gmail.com>
2020-06-15 18:24:02 +0530

    bug fix : auth

---
+++ middleware/auth.js
@@ -0,0 +1,29 @@
+const jwt     = require( 'jsonwebtoken' ) ;
+const respond = require( '../response.js' ) ;
+const User    = require( '../app/user/user.model.js' ) ;
+
+const auth = async( req, res, next ) => {
+    console.log( '\n\n' )
+    console.log( { req: req.body } ) ;
+    try {
+        const token = req.header( 'Authorization' ) ;
+        const _id = jwt.verify( token, process.env.JWT_KEY ) ;
+        const user = await User.findOne( { _id: _id } ) ;
+        if ( !user ) {
+            console.log( 'Invalid Token') ;
+            throw new Error();
+        }
+        console.log( 'Token Verified' ) ;
+        req.body.UserID = user._id ;
+        req.body.userDoc = user ;
+        return next();
+    } catch ( err ) {
+        if( req.url === '/user/login' ) {
+            return next() ;
+        }else{
+            respond.err( res, respond.errData.invalidToken ) ;
+        }
+    }
+}
+
+module.exports = auth;
