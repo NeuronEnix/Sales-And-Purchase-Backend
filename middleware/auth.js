c0862dbfbcc2c95979d6721a575ff5449ed10391
neuron909 <kaushikrb909@gmail.com>
2020-06-17 21:32:19 +0530

    code restructure

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,27 +3,26 @@ const respond = require( '../response.js' ) ;
 const User    = require( '../app/user/user.model.js' ) ;
 let ID = 1;
 const auth = async( req, res, next ) => {
-    console.log( '\n\n' ) ;
-    console.log( { ID : ID, URL : req.url, Method : req.method, Body : req.body, } ) ;
-    res.ID = ID++ ;    res.TS = Date.now() ;
-    if ( toString( req.method ) == 'OPTIONS' ) return respond.ok( res ) ;
     try {
+        console.log( '\n\n' ) ;
+        console.log( { ID : ID, URL : req.url, Method : req.method, Body : req.body, } ) ;
+        res.ID = ID++ ;    res.TS = Date.now() ;
+
+        if ( toString( req.method ) == 'OPTIONS' ) return respond.ok( res ) ;
+
         const token = req.header( 'Authorization' ) ;
         const _id = jwt.verify( token, process.env.JWT_KEY ) ;
+
         const user = await User.findOne( { _id: _id } ) ;
-        if ( !user ) {
-            console.log( 'Invalid Token') ;
-            throw new Error();
-        }
-        console.log( 'Token Verified' ) ;
+
+        if ( !user ) throw { err : respond.errData.invalidToken } ;
+
         req.user = user ;
         return next();
     } catch ( err ) {
-        if( req.url === '/user/login' ) {
-            return next() ;
-        }else{
-            respond.err( res, respond.errData.invalidToken ) ;
-        }
+
+        if( req.url === '/user/login' ) return next() ;
+        else return respond.err( res, err ) ;
     }
 }
 
