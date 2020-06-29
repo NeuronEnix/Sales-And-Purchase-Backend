c59564c82738f79621668bfc0602d9d6c9cf9e05
neuron909 <kaushikrb909@gmail.com>
2020-06-29 11:58:05 +0530

    added validation to sales and purchases

---
+++ app/sale/sale.validator.js
@@ -0,0 +1,25 @@
+const Joi = require( '@hapi/joi' ) ;
+
+const respond = require( '../../response.js' ) ;
+const errData = respond.errData ;
+
+const Items = Joi.object().keys({
+    Name : Joi.string().trim().min(2).max(10).required(),
+    Qty : Joi.number().positive().required()
+})
+
+const itemSchema = Joi.array().items( Items ) ;
+
+const createSchema = Joi.object({
+    Items      : Joi.array().min( 1 ).required()
+})
+module.exports.create = async ( req, res, next ) => {
+    console.log( 'validation sale')
+    try { 
+        const createSchemaValidation = createSchema.validateAsync( req.body     ) ;
+        const itemSchemaValidation   = itemSchema.validateAsync( req.body.Items ) ;
+        await createSchemaValidation ;
+        await itemSchemaValidation   ;
+        return next() ;
+    } catch ( err ) { console.log( err) ; return respond.err( res, { err : errData.validationErr, info : err.details[0].message } ) ; }
+} ;
