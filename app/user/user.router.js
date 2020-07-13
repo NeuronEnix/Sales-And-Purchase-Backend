73339745b8ccd86ad2e043a5f81d40709bfb42d6
NEURON <50287102+neuron909@users.noreply.github.com>
2020-07-14 00:57:14 +0530

    added user logout route

--- app/user/user.router.js
+++ app/user/user.router.js
@@ -4,5 +4,6 @@ const validate = require( './user.validator.js' ) ;
 
 router.post( '/login'  , validate.login  , user.login  ) ;
 router.post( '/signup' , validate.signup , user.signup ) ;
+router.post( '/logout' ,                   user.logout ) ;
 
 module.exports.router = router
