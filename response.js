b76a827df5e559f155f66cc74a3ac27160d5e5de
NEURON <50287102+neuron909@users.noreply.github.com>
2020-07-14 00:57:55 +0530

    bug fix : response time

--- response.js
+++ response.js
@@ -3,7 +3,7 @@ module.exports.ok = ( resObj, data ) => {
         code : 0,
         data : data
     };
-    console.log( { ID : resObj.ID, time : (Date.now() - resObj.TS) + "ms", res : res, } ) ;
+    console.log( { ID : resObj.ID, time : (Date.now() - resObj._TS) + "ms", res : res, } ) ;
     resObj.status( 200 ).send( res ) ;
 }
 
@@ -17,13 +17,13 @@ module.exports.err = ( resObj, err ) => {
         res = this.errData.unknownErr ;
         console.log( err ) ;
     } finally {
-        console.log( { ID : resObj.ID, time : (Date.now() - resObj.TS) + "ms", res : res, } ) ;
+        console.log( { ID : resObj.ID, time : (new Date() - resObj._TS) + "ms", res : res, } ) ;
         resObj.status( 400 ).send( res ) ;
     }
 }
 
 module.exports.errData = {
-    unknownErr          : { code : -1 , err : 'Unknown Error!'                  },
+    unknownErr          : { code : -1 , err : 'Unknown Error!', info : 'Unknown Error - Inform Developer Immediately!' },
     resNotFound         : { code : 1  , err : 'Resource Not Found'              },
     invalidToken        : { code : 2  , err : 'Invalid Token'                   },
     invalidCredential   : { code : 3  , err : 'Incorrect Credential'            },
@@ -31,10 +31,13 @@ module.exports.errData = {
     duplicateErr        : { code : 5  , err : 'Value Already Exist (Duplicate)' },
     validationErr       : { code : 6  , err : 'Validation Error'                },
     outOfStock          : { code : 7  , err : 'Not Enough Stock'                },
+    AccessTokenExpired  : { code : 8  , err : 'Access Token Expired'            },
+    RefreshTokenExpired : { code : 9  , err : 'Refresh Token Expired'           },
+    unAuthorized        : { code : 10 , err : 'Not Authorized'                  },
 } ;
 
 module.exports.defRes = ( resObj, apiName ) => {
     const res = { code : -1, status : 'FAILED', err : 'In Progress', api : apiName }
-    console.log( { ID : resObj.ID, time : (Date.now() - resObj.TS) + "ms", res : res, } ) ;
-    resObj.status( 400 ).send( res ) ;
+    console.log   ( { ID : resObj.ID, time : (Date.now() - resObj._TS) + "ms", res : res, } ) ;
+    resObj.status ( 400 ).send( res ) ;
 }
