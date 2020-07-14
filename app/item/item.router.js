0b877971a84c911e0a9bef0dd24da1507837d42b
NEURON <50287102+neuron909@users.noreply.github.com>
2020-07-14 14:18:55 +0530

    added : search route

--- app/item/item.router.js
+++ app/item/item.router.js
@@ -1,13 +1,12 @@
 const router = require( 'express' ).Router() ;
 
-const item = require( './item.controller' ) ;
-const logger = require( './item.logger.js' ) ;
+const item     = require( './item.controller'   ) ;
+const logger   = require( './item.logger.js'    ) ;
 const validate = require( './item.validator.js' ) ;
 
-router.get( '/'        , item.search   ) ;
-
-router.post( '/add'    , validate.add    , item.add    , logger.add ) ;
-router.post( '/update' , validate.update , item.update , logger.update ) ;
-router.post( '/detail' , validate.detail , item.detail , ) ;
+router.post( '/'       , validate.search , item.search                  ) ;
+router.post( '/add'    , validate.add     , item.add    , logger.add    ) ;
+router.post( '/update' , validate.update  , item.update , logger.update ) ;
+router.post( '/detail' , validate.detail  , item.detail ,               ) ;
 
 module.exports.router = router
