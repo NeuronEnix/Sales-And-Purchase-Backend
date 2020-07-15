ab9301edb46aabc22ca14d0a4097606ebb1f7d81
NEURON <50287102+neuron909@users.noreply.github.com>
2020-07-15 06:15:45 +0530

    bug fix : undefined ID

--- app/seller/seller.controller.js
+++ app/seller/seller.controller.js
@@ -3,7 +3,7 @@ const respond = require( '../../response.js' ) ;
 
 module.exports.add = async ( req, res ) => {
     try{
-        req.body.UserID = req.user._id ;
+        req.body.UserID = req.UserID ;
         const sellerData = req.body ;
         await Seller.AddNewSeller( sellerData ) ;
         respond.ok( res ) ;
