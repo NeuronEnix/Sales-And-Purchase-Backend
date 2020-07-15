fb459b2e2ed2a9c51612aab950b06f8ff130661f
NEURON <50287102+neuron909@users.noreply.github.com>
2020-07-15 17:11:51 +0530

    Fix : Mongoose Indexing

--- connection.js
+++ connection.js
@@ -5,6 +5,14 @@ mongoose.set( 'useNewUrlParser'    , true  ) ;
 mongoose.set( 'useFindAndModify'   , false ) ;
 mongoose.set( 'useCreateIndex'     , true  ) ;
 mongoose.set( 'useUnifiedTopology' , true  ) ;
+mongoose.set( 'autoIndex'          , true  ) ;
+
+// Importing schema 
+require( './app/item/item.model.js'         ) ;
+require( './app/user/user.model.js'         ) ;
+require( './app/sale/sale.model.js'         ) ;
+require( './app/seller/seller.model.js'     ) ;
+require( './app/purchase/purchase.model.js' ) ;
 
 // Connects to DB
 module.exports.connect = () => {
@@ -12,4 +20,3 @@ module.exports.connect = () => {
         .then  ( val => { console.log('Connected to DB')     ; } )
         .catch ( err => { console.log('Not Connected to DB') ; } ) ;
 }
-
