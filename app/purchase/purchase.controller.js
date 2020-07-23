c5e120a7df363f4ff825d921f5c50fad23613695
NEURON <50287102+neuron909@users.noreply.github.com>
2020-07-23 10:35:12 +0530

    Added : Purchase history (More Info )

--- app/purchase/purchase.controller.js
+++ app/purchase/purchase.controller.js
@@ -15,12 +15,44 @@ module.exports.detail = async ( req, res ) => {
 }
 
 module.exports.listMy = async ( req, res ) => {
+    const pageNo = req.body.P;
     const filter  = { UserID:req.UserID } ;
     const project = { _id:1 } ;
-    return respond.ok( res, await Purchase.List( filter, project ) ) ;
+    const doc = await Purchase.List( filter, project, pageNo ) ;
+    data = [];
+    for( d of doc ) {
+        data.push({
+            _id:d._id,
+            SellerName : 'Seller'+pageNo,
+            ItemCount : 34,
+        })
+    }
+    return respond.ok( res, data ) ;
 }
 
 module.exports.listAll = async ( req, res ) => {
+    const pageNo = req.body.P;
     const project = { _id:1 } ;
-    return respond.ok( res, await Purchase.List( {}, project ) ) ;
+    let doc = await Purchase.List( {}, project, pageNo )
+    data = [];
+    for( d of doc ) {
+        data.push({
+            _id:d._id,
+            UserName : 'User1',
+            SellerName : 'Seller'+pageNo,
+            ItemCount : 34,
+        })
+    }
+    
+    return respond.ok( res, data ) ;
+}
+
+module.exports.update = async ( req, res ) => {
+    console.log( await Purchase.findOneAndUpdate( { _id:req.body._id }, { $set : { Items:req.body.Items } } ) ) ;
+    return respond.ok( res ) ;
+}
+
+module.exports.delete = async ( req, res ) => { 
+    console.log( await Purchase.findByIdAndDelete( req.body._id ) ) ;
+    return respond.ok( res ) ;
 }
--- app/purchase/purchase.router.js
+++ app/purchase/purchase.router.js
@@ -3,8 +3,10 @@ const purchase = require( './purchase.controller.js' ) ;
 const validate = require( '../../validator.js' ) ;
 
 router.post( '/create' , validate.purchase.create ,  purchase.create ) ;
-router.post( '/detail' , purchase.detail ) ;
-router.get ( '/listMy' , purchase.listMy  ) ;
-router.get ( '/listAll', purchase.listAll ) ;
+router.post( '/detail' , purchase.detail  ) ;
+router.post( '/update' , purchase.update  ) ;
+router.post( '/delete' , purchase.delete  ) ;
+router.post( '/listMy' , purchase.listMy  ) ;
+router.post( '/listAll', purchase.listAll ) ;
 
 module.exports.router = router
